061966b76ad167c1ce76c7e9ef8f31959da62391
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-10-11 04:16:47 -0700

    pending

---
+++ app.js
@@ -0,0 +1,28 @@
+
+function menu() {
+
+    let nav_2 = document.getElementById('nav_2');
+    let menu = document.getElementById('parent_4');
+    let cut = document.getElementById('parent_5');
+
+    nav_2.style.display = 'none';
+    menu.style.display = 'none';
+    cut.style.display = 'block';
+
+    // console.log('alsdjfolsdjkfl');
+    
+}
+
+function cut() {
+
+    let nav_2 = document.getElementById('nav_2');
+    let menu = document.getElementById('parent_4');
+    let cut = document.getElementById('parent_5');
+
+    nav_2.style.display = 'block';
+    menu.style.display = 'block';
+    cut.style.display = 'none';
+
+    // console.log('alsdjfolsdjkfl');
+    
+}
